cea298a6554a61e5be4b1e2479c659d4d09d178f
elijah-whittle <edw2290@nyu.edu>
2021-11-23 14:11:49 -0500

    add Joystick

--- Touch-Demo/Assets/Code/FloatyBox.cs
+++ Touch-Demo/Assets/Code/FloatyBox.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class FloatyBox : MonoBehaviour
 {
 
-    public float moveForce; // how fast to move
+    public int moveSpeed; // how fast to move
 
     public float jumpForce;
     public float fallForce;
     
     public LayerMask groundLayer;
     public Transform feet;
+
+    public FloatingJoystick joystick;
 
     private bool grounded = false;  // touching ground?
     private bool seenJump = false;  // indicates whether a jump has happened - used to avoid multiple jump touch strokes during liftoff
@@ -25,14 +27,19 @@ public class FloatyBox : MonoBehaviour
 
     void Update()
     {
+        // move
+        float xSpeed = joystick.Horizontal * moveSpeed;
+        _rigidbody.velocity = new Vector2(xSpeed, _rigidbody.velocity.y);
+
         for (int i = 0; i < Input.touchCount; ++i)
         {
             // get ground collision
             grounded = Physics2D.OverlapCircle(feet.position, .3f, groundLayer);
 
             Touch touch = Input.GetTouch(i);
-            if (touch.phase == TouchPhase.Moved)
+            if (touch.phase == TouchPhase.Moved && touch.position.x > Screen.width / 2)
             {
+                // jump or fall
                 if (Mathf.Abs(touch.deltaPosition.y) > Mathf.Abs(touch.deltaPosition.x))
                 {
                     // jump (1) or fall (0)
@@ -40,26 +47,22 @@ public class FloatyBox : MonoBehaviour
                     if (sign && !seenJump && grounded)
                     {
                         // jump or fall
-                        _rigidbody.AddForce(Vector2.up * jumpForce);
+                        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+                        //_rigidbody.AddForce(Vector2.up * jumpForce);
                         //touch.phase = TouchPhase.Canceled;
                         seenJump = true;
                         StartCoroutine(resetJump(1f));
                     }
                     if (!fallGrace && !sign && !grounded)
                     {
-                        _rigidbody.AddForce(Vector2.down * jumpForce);
+                        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, -fallForce);
+                        //_rigidbody.AddForce(Vector2.down * jumpForce);
                         fallGrace = true;
                         StartCoroutine(resetFall(.2f));
                     }
                 }
-                else
-                {
-                    // move
-                    int sign = (touch.deltaPosition.x > 0) ? 1 : -1;
-                    _rigidbody.AddForce(sign * Vector2.right * moveForce);
-                }
             }
-        }
+        } // for
     }
 
     // resets jump grace period
